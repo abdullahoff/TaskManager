60d686356f401acb8351f8baca1fbd4537b0c100
Abdullah <abdullah2004@live.ca>
2021-09-21 01:31:47 -0400

    finished project + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,12 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { Router, RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
-  {path: '', component: TasksComponent}
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
 ]
 
 
@@ -27,7 +30,9 @@ const appRoutes: Routes = [
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
     
     
   ],
